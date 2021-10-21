f2a8469eff0e6972db6be218cfbfc80ca9ad8c48
Naturalkidv1 <eltiburonjuanjo@hotmail.com>
2021-10-21 16:50:46 +0200

    Se realiza commit en todos los EJs y en el pom.xml

---
+++ ejercicios_UT03_estructuras_repeticion/src/main/java/ejercicios_ut03_estructuras_repeticion/Ej08.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios_ut03_estructuras_repeticion;
+
+/**
+ *
+ * @author Naturalkid
+ */
+public class Ej08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
